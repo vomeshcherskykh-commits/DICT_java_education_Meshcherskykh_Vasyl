9474810cdb3d4d9ac5e52ae0da669017bdb25bb6
MeshcherskykhVasyl <v.o.meshcherskykh@student.khai.edu>
2025-10-05 20:56:39 +0300

    ChatBot 4-rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -10,6 +10,7 @@ public class ChatBot {
         stage1();
         stage2(scanner);
         stage3(scanner);
+        stage4(scanner);
 
         scanner.close();
     }
@@ -35,5 +36,14 @@ public class ChatBot {
 
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+    public static void stage4(Scanner scanner) {
+        System.out.println("Now I will prove to you that I can count to any number you want!");
+        int num = scanner.nextInt();
+
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i + "!");
+        }
     }
 }
