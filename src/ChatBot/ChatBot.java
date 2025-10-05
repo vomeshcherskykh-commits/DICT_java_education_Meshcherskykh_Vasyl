13415605b23e2c09856264a2236916ac50c8f8ee
MeshcherskykhVasyl <v.o.meshcherskykh@student.khai.edu>
2025-10-05 20:42:39 +0300

    ChatBot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is BOT.");
+        System.out.println("I was created in 2025.");
+    }
+}
