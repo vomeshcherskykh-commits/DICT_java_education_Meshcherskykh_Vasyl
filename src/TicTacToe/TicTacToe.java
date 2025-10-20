e85a2fb8760cc6b35f627993df0c97f43650a6b3
MeshcherskykhVasyl <v.o.meshcherskykh@student.khai.edu>
2025-10-20 21:45:47 +0300

    TicTacToe stage 2

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,9 +1,22 @@
 package TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(input.charAt(i * 3 + j) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
