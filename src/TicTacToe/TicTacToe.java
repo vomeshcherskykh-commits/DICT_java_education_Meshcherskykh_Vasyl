95f141d0eca169a05bf27eec2fd38689f13829d6
MeshcherskykhVasyl <v.o.meshcherskykh@student.khai.edu>
2025-10-20 21:51:50 +0300

    TicTacToe stage 4

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -15,7 +15,41 @@ public class TicTacToe {
         }
 
         printField(field);
-        System.out.println(analyze(field));
+
+        while (true) {
+            System.out.print("Enter the coordinates: ");
+            String line = scanner.nextLine();
+            String[] parts = line.split(" ");
+
+            if (parts.length != 2) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+
+            int x, y;
+            try {
+                x = Integer.parseInt(parts[0]);
+                y = Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("You should enter numbers!");
+                continue;
+            }
+
+            if (x < 1 || x > 3 || y < 1 || y > 3) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+
+            if (field[x - 1][y - 1] != '_') {
+                System.out.println("This cell is occupied! Choose another one!");
+                continue;
+            }
+
+            field[x - 1][y - 1] = 'X';
+            break;
+        }
+
+        printField(field);
     }
 
     private static void printField(char[][] field) {
@@ -28,37 +62,5 @@ public class TicTacToe {
             System.out.println("|");
         }
         System.out.println("---------");
-    }
-
-    private static String analyze(char[][] field) {
-
-        int xCount = 0;
-        int oCount = 0;
-        for (char[] row : field) {
-            for (char c : row) {
-                if (c == 'X') xCount++;
-                if (c == 'O') oCount++;
-            }
-        }
-
-        boolean xWins = checkWin(field, 'X');
-        boolean oWins = checkWin(field, 'O');
-
-        if ((xWins && oWins) || Math.abs(xCount - oCount) > 1) return "Impossible";
-        if (xWins) return "X wins";
-        if (oWins) return "O wins";
-        if (xCount + oCount == 9) return "Draw";
-        return "Game not finished";
-    }
-
-    private static boolean checkWin(char[][] field, char player) {
-
-        for (int i = 0; i < 3; i++) {
-            if (field[i][0] == player && field[i][1] == player && field[i][2] == player) return true;
-            if (field[0][i] == player && field[1][i] == player && field[2][i] == player) return true;
-        }
-
-        return (field[0][0] == player && field[1][1] == player && field[2][2] == player)
-                || (field[0][2] == player && field[1][1] == player && field[2][0] == player);
     }
 }
