c2f1b214d323ea7f757f325913dc86b476fd5eb6
MeshcherskykhVasyl <v.o.meshcherskykh@student.khai.edu>
2025-10-20 21:43:32 +0300

    ÐTicTacToe stage 1

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,9 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
+    }
+}
